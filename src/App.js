5da8909276c1d08f703029f3cf8e150db8ffd201
Susanna Zandonai <susanna.zandonai@gmail.com>
2017-09-09 15:48:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const BASE = "max-app";
+
+class App extends Component {
+  render() {
+    return (
+      <div className={`${BASE}`}>
+        <div className={`${BASE}__header`}>
+          <div className={`${BASE}__containerHelmMenu`}>
+            <div className={`${BASE}__helmOptions`}>
+              <div className={`${BASE}__helmPhotos`}></div>
+              <div className={`${BASE}__helmContacts`}></div>
+            </div>
+          </div>
+        </div>
+        <div className={`${BASE}__main`}></div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
