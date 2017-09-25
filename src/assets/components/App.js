4c7d44fbaf704b5de2d645ea6ee57e0027a41e2c
Nico Santoro <nicosantoro.26@gmail.com>
2017-09-25 19:30:50 +0100

    Rejected react-default-app; Improved Sidebar behaviour;

--- src/assets/components/App.js
+++ src/assets/components/App.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Icon, Image, Sidebar, Segment, Menu } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import '../index.css';
 import './App.css';
 
 const BASE = "max-app";
@@ -15,11 +16,42 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      posts: [],
+      sidebarVisible: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
   }
 
   componentDidMount() {
-    console.log(this.props.section);
+    const URL = 'http://143.239.97.138:8080/Aqvarium/ws/postService/getAll';
+    const OPTIONS = { method: 'GET' };
+
+    fetch(URL, OPTIONS)
+      .then(response => Promise.all([response, response.json()]))
+      .then(([response, json]) => {
+        if (response.ok) {
+          if (json instanceof Array) {
+            this.setState({
+              posts: json
+            });
+            return;
+          }
+          return;
+        }
+        const errorMessage = json.errorMessage ? json.errorMessage : 'Unexpected error';
+        Promise.reject(errorMessage);
+      })
+      .catch(err => Promise.reject(err));
+  }
+
+  toggleMenu(e) {
+    e.preventDefault();
+    this.setState(({ sidebarVisible }) => {
+      return {
+        sidebarVisible: !sidebarVisible
+      };
+    });
   }
 
   render() {
@@ -27,40 +59,62 @@ class App extends Component {
       <div className={`${BASE}`}>
         <div className={`${BASE}__header`}>
           <div className={`${BASE}__headerText`}>
-            aqvarivm
-          </div>
-          <div className={`${BASE}__headerNavigation`}>
-            <div className={`${BASE}__navigationItem ${BASE}__navigationItem--gallery`}>
-              <i className="fa fa-anchor" aria-hidden="true"></i>
-            </div>
-            <div className={`${BASE}__navigationItem ${BASE}__navigationItem--contacts`}>
-              <i className="fa fa-commenting" aria-hidden="true"></i>
-            </div>
+            <button className={`${BASE}__toggleMenu`} onClick={this.toggleMenu}>
+              <Icon name='list layout' />
+            </button>
+            <h1>aqvarivm</h1>
           </div>
         </div>
         <div className={`${BASE}__main`}>
-        {/*<Card>
-          <Image src="" />
-          <Card.Content>
-            <Card.Header>
-              The Aqvarivm
-            </Card.Header>
-            <Card.Meta>
-              <span className='date'>
-                Posted in 2015
-              </span>
-            </Card.Meta>
-            <Card.Description>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac vestibulum leo.
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name='unhide' />
-              View more
-            </a>
-          </Card.Content>
-        </Card>*/}
+        <Sidebar.Pushable as={Segment} className={`${BASE}__pushable`}>
+          <Sidebar as={Menu} animation='push' width='thin' visible={this.state.sidebarVisible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='user'>
+              <Icon name='user' />
+              Account
+            </Menu.Item>
+            <Menu.Item name='setting'>
+              <Icon name='setting' />
+              Settings
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher className={`${BASE}__pusher`}>
+            {
+              !this.state.posts.length
+              ? null
+              : this.state.posts.map(item => (
+                <Card className={`${BASE}__card`} key={item.id}>
+                  <Image
+                    src="/static_assets/acquario.jpg"
+                    href="#"
+                  />
+                  <Card.Content>
+                    <Card.Header>
+                      {item.title}
+                    </Card.Header>
+                    <Card.Meta>
+                      <span className='date'>
+                        {item.dtIns}
+                      </span>
+                    </Card.Meta>
+                    <Card.Description>
+                      {`Pic: ${item.pic}`}
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='unhide' />
+                      View more
+                    </a>
+                  </Card.Content>
+                </Card>
+              ))
+            }
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
         </div>
       </div>
     );
